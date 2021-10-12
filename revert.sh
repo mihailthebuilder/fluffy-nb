be876ec376d04828e829bfc8a9dfc8e0478deea5
Mihail Marian <mihailmarian12@gmail.com>
2021-10-12 16:47:32 +0100

    comments and mkdir

--- revert.sh
+++ revert.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
+# Reverts to previous state of img folder using the fluffy-images archive.
+# Useful when you've accidentally tampered with the img folder
+
 mkdir -p img/combined
 
 tar -xf fluffy-images.tar.xz -C ./img/combined
+
+cd img
+mkdir fluffy not_fluffy
 
 testing="hullo"
 
