da4ad7e2405985754566d40bfec4a0c89b47f5f0
Mihail Marian <mihailmarian12@gmail.com>
2021-10-12 15:43:45 +0100

    comments

--- img-prep.sh
+++ img-prep.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-cd ./img/fluffy
+# removed combined images folder
+cd img
+rm -rf combined
+
+# convert all fluffy image names to random lowercase text
+cd ./fluffy
 
 for file in * ; do
     basename=$(tr -dc a-z </dev/urandom | head -c 20)
@@ -8,6 +13,7 @@ for file in * ; do
 	mv "$file" "$newname";
 done
 
+# convert all non-fluffy image names to random uppercase text
 cd ../not_fluffy
 
 for file in * ; do
